2e6f84f04efb3721e67cfbb07b70973f7d331273
raul sanchez <brawlprorey79907@hotmail.com>
2025-02-02 10:29:56 -0700

    changed tab icon sized

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -8,13 +8,13 @@ export default function TabLayout() {
       screenOptions={{ tabBarStyle: { backgroundColor: "#121212" }, tabBarInactiveTintColor: "#888", headerStyle: { backgroundColor: "#121212" }, headerTintColor: "#fff"}}>
 
       {/* workout tab */}
-      <Tabs.Screen name="workout" options={{ title: "Workout", tabBarIcon: ({ focused }) => ( <Image source={require("../../assets/images/dumbell.png")} style={{ width: 30,  height: 30}}/>)}}/>
+      <Tabs.Screen name="workout" options={{ title: "Workout", tabBarIcon: ({ focused }) => ( <Image source={require("../../assets/images/dumbell.png")} style={{ width: 40,  height: 35}}/>)}}/>
 
       {/* home tab */}
-      <Tabs.Screen name="home" options={{ title: "Meal Input", tabBarIcon: ({ focused }) => (<Image source={require("../../assets/images/mealInput.png")} style={{ width: 30, height: 30 }} />)}}/>
+      <Tabs.Screen name="home" options={{ title: "Meal Input", tabBarIcon: ({ focused }) => (<Image source={require("../../assets/images/mealInput.png")} style={{ width: 50, height: 35 }} />)}}/>
 
       {/* user tab */}
-      <Tabs.Screen name="user" options={{ title: "Account", tabBarIcon: ({ focused }) => (<Image source={require("../../assets/images/user.png")} style={{ width: 30, height: 30 }} />)}}/>
+      <Tabs.Screen name="user" options={{ title: "Account", tabBarIcon: ({ focused }) => (<Image source={require("../../assets/images/user.png")} style={{ width: 40, height: 45 }} />)}}/>
 
     </Tabs>
   );
