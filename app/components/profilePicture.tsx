c80839fa1f0a7fa944f1015eb81eae9bb1a9f1bb
raul sanchez <brawlprorey79907@hotmail.com>
2025-02-07 22:41:32 -0700

    comments

--- app/components/profilePicture.tsx
+++ app/components/profilePicture.tsx
@@ -7,20 +7,28 @@ import { db } from "@/firebaseConfig";
 import { useUser } from "@/app/userContext";
 
 export default function ProfilePicture() {
-  const { userId } = useUser();
-  const router = useRouter();
-  const [profilePicture, setProfilePicture] = useState<string | null>(null);
+  const { userId } = useUser(); //get the user id from the context
+  const router = useRouter(); //hook for navigating between screens
+  const [profilePicture, setProfilePicture] = useState<string | null>(null); //state to store pfp
 
+  /**
+   * fetch the pfp when the user logs in
+   */
   useEffect(() => {
     if (userId) {
       fetchProfilePicture();
     }
   }, [userId]);
 
+  /**
+   *fetch the pfp from firestore
+   */
   const fetchProfilePicture = async () => {
     try {
-      const userRef = doc(db, "users", String(userId));
-      const userSnap = await getDoc(userRef);
+      const userRef = doc(db, "users", String(userId)); //reference to the users document in db
+      const userSnap = await getDoc(userRef); //fetch the document
+
+      //if user exists, set the pfp
       if (userSnap.exists()) {
         setProfilePicture(userSnap.data().profilePicture || null);
       }
@@ -29,10 +37,14 @@ export default function ProfilePicture() {
     }
   };
 
+  /**
+   * re fetch when screen gains focus
+   * this ensures the profile picture is updated on screen when changed
+   */
   useFocusEffect(
     useCallback(() => {
       if(userId) {
-        fetchProfilePicture();
+        fetchProfilePicture(); //fetch profile picture when the screen comes into focus
       }
     }, [userId])
   );
@@ -54,14 +66,14 @@ export default function ProfilePicture() {
 const styles = StyleSheet.create({
   avatarContainer: {
     position: "absolute",
-    top: 40, //adjust for safe area
-    right: 16,
+    top: 40, //position below search bar
+    right: 16, //top right
     zIndex: 1000, //ensure it stays on top
   },
   avatar: {
     width: 40,
     height: 40,
-    borderRadius: 20,
+    borderRadius: 20, //pfp is circle
     borderWidth: 1,
     borderColor: "#BB86FC",
   },
